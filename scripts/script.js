916c0ede787295fa8c07dd3c867b73de55fed18b
Ujwal N K <ujwal.nk@gmail.com>
2022-09-25 23:53:04 +0530

    Update for From Control

--- scripts/script.js
+++ scripts/script.js
@@ -14,19 +14,32 @@ function convert() {
     }
 
     let output = document.getElementById("result");
-
     let checkVal = Math.trunc(userInput * Math.pow(10, percision));
 
     output.innerText = "";
+    
+    // Check for precision value
+    if(Number(document.getElementById("userInput").value).countDecimals() < document.getElementById("precisionInput").value || document.getElementById("precisionInput").value < 0){
+        output.innerHTML = "$$Check \\hspace{5px} Precision$$";
+        MathJax.typeset();
+        reset();
+
+        // Highlight the precision box
+        document.getElementById("precisionInput").className = document.getElementById("precisionInput").className + " is-invalid";
+        console.log(document.getElementById("precisionInput").className);
+        return;
+    }
+
     document.getElementById("userInteractionButton").style.display = "none";
     let progress = document.getElementById("userInteractionProgress");
     progress.style.display = "block";
 
-    for (let x = minSqrt; x < sqrt; x++) {
+    for (let x = minSqrt; x < sqrt; x ++) {
+        // Check for the rational value
         progress.children[0].setAttribute("aria-valuenow", (x / sqrt) * 100);
         if (checkVal == Math.trunc(Math.sqrt(x) * Math.pow(10, percision))) {
             if (negativeInput == 1) {
-                output.innerHTML = "$$-\\sqrt{" + x + "}$$";
+                output.innerHTML = "$$\\-sqrt{" + x + "}$$";
             } else {
                 output.innerHTML = "$$\\sqrt{" + x + "}$$";
             }
@@ -34,10 +47,13 @@ function convert() {
             reset();
             return;
         }
-        for (let y = minNumeric; y < numeric; y++) {
+
+        for (let y = minNumeric; y < numeric; y ++) {
+        // Check for irrational number
+
             if (checkVal == Math.trunc(Math.sqrt(x) * Math.pow(10, percision)) + y * Math.pow(10, percision)) {
                 if (negativeInput == 1) {
-                    output.innerHTML = "$$-\\(sqrt{" + x + "} +" + y + ")$$";
+                    output.innerHTML = "$$-(\\sqrt{" + x + "} +" + y + ")$$";
 
                 } else {
                     output.innerHTML = "$$\\sqrt{" + x + "} +" + y + "$$";
@@ -48,8 +64,7 @@ function convert() {
             }
             else if (checkVal == Math.trunc(Math.sqrt(x) * Math.pow(10, percision)) - y * Math.pow(10, percision)) {
                 if (negativeInput == 1) {
-                    output.innerHTML = "$$\\-(sqrt{" + x + "} -" + y + ")$$";
-
+                    output.innerHTML = "$$-(\\sqrt{" + x + "} -" + y + ")$$";
                 }
                 else {
                     output.innerHTML = "$$\\sqrt{" + x + "} -" + y + "$$";
@@ -80,4 +95,35 @@ function convert() {
 function reset() {
     document.getElementById("userInteractionButton").style.display = "block";
     document.getElementById("userInteractionProgress").style.display = "none";
+}
+
+function help(){
+    // Generate a random sign
+    let signX = (Math.random() > 0.5) ? 1 : -1;
+    let signY = (Math.random() > 0.5) ? 1 : -1;
+
+    // Generate random Number
+    let a = signX * Math.random()*1000 + signY * Math.sqrt(Math.random()*1000);
+    document.getElementById("userInput").value = a;
+
+    // Set the range values from 0 to 1001
+    document.getElementById("numericMinInput").value = 0;
+    document.getElementById("numericInput").value = 1001;
+    document.getElementById("sqrtMinInput").value = 0;
+    document.getElementById("sqrtInput").value = 1001;
+
+    // Set the precision to less that number of decimal places
+    document.getElementById("precisionInput").value = 2;
+
+    // Click on Simplify Button
+    document.getElementById("userInteractionButton").click();
+}
+
+function setMaxPrecision(element){
+    document.getElementById("precisionInput").max = Number(element.value).countDecimals();
+}
+
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
 }
